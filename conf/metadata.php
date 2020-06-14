b7a349c54c9f8ddbb8fba845d37ada83eecc1d3f
Soarinferret <thecodye@kanto.cloud>
2020-06-14 15:39:08 -0500

    Add ability to include logo in top left navbar

--- conf/metadata.php
+++ conf/metadata.php
@@ -8,3 +8,4 @@ $meta['discussionPage']   = array('string');
 $meta['userPage']         = array('string');
 $meta['hideTools']        = array('onoff');
 $meta['movePageTools']    = array('onoff'); 
+$meta['showIcon']         = array('onoff');
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -8,3 +8,4 @@ $lang['discussionPage']   = 'Discussion page (leave empty to disable discussions
 $lang['userPage']         = 'User page (leave empty to disable user pages)';
 $lang['hideTools']        = 'Hide tools when not logged in?';
 $lang['movePageTools']    = 'Move page tools to top of page instead of sidebar'; 
+$lang['showIcon']         = 'Show icon in top left screen';
