3d4a563478840e0c07c6bce8b78bd8289c5bd3d9
SiM <simon.wirth@gmail.com>
2020-07-29 12:00:47 +0200

    Update fr translation (#34)

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -7,3 +7,5 @@
 $lang['discussionPage']   = 'Page de discussion (effacer désactive les discussions)';
 $lang['userPage']         = 'Page utilisateur (effacer désactive les pages utilisateurs)';
 $lang['hideTools']        = 'Masquer les outils aux visiteurs anonymes ?';
+$lang['movePageTools']    = 'Déplacer les outils de page en haut de la page au lieu du panneau latéral'; 
+$lang['showIcon']         = "Afficher l'icône en haut à gauche de l'écran";
