1ebb50a87957c5375a027ad1167f57525abef078
tangdou1 <35254744+tangdou1@users.noreply.github.com>
2020-09-07 15:40:44 +0800

    Add files via upload

---
+++ lang/zh/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Chinese (Simplified) language file for config
+ *
+ */
+
+$lang['discussionPage']   = '讨论页面 (留空以关闭讨论)';
+$lang['userPage']         = '用户页面 (留空以关闭用户页面)';
+$lang['hideTools']        = '当未登录时隐藏工具?';
+$lang['movePageTools']    = '移动页面工具到页面顶部而不是侧边栏'; 
+$lang['showIcon']         = '在屏幕左上方显示图标';
