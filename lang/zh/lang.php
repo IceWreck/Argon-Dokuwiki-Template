d4740b4ab94bbf631eb7ff4c7d435794314f7100
tangdou1 <35254744+tangdou1@users.noreply.github.com>
2020-09-13 13:45:27 +0800

    Update Chinese  translations (#40)
    * Update lang.php
    
    * Add files via upload

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Chinese (Simplified) language file
+ * Chinese (Simplified) language file for template
  *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
-$lang['discussion']      = '讨论';
-$lang['back_to_article'] = '回到文章';
-$lang['userpage']        = '用户页面';
-
-//Setup VIM: ex: et ts=2 :
+$lang['discussion']        = '讨论';
+$lang['back_to_article']   = '回到文章';
+$lang['userpage']          = '用户页面';
+$lang['__existing__']      = '已存在页面超链接的颜色';
+$lang['__missing__']       = '不存在页面超链接的颜色';
+$lang['__site_width__']    = '全站宽度(单位可以是: %, px, em, ...)';
+$lang['__sidebar_width__'] = '侧边栏宽度(如果有的话) (单位可以是: %, px, em, ...)';
